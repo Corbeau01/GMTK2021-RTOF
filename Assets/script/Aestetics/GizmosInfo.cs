594621a952fca9d2fbcd6f2b4ae63f8a6aea3cf6
Richard <rail.rich@gmail.com>
2021-06-12 12:12:10 -0400

    Level1

---
+++ Assets/script/Aestetics/GizmosInfo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmosInfo : MonoBehaviour
+{
+	public BoxCollider2D Collider2D;
+	public Color Color;
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color;
+		Gizmos.DrawCube(Collider2D.transform.position, new Vector3(Collider2D.size.x * transform.lossyScale.x, Collider2D.size.y * transform.lossyScale.y, 1));
+	}
+}
