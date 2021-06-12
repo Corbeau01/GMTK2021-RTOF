df921da294183fab871b9426fb939eb1ceeeaa5e
Corbeau01 <corvus-corax@hotmail.com>
2021-06-12 00:24:47 -0400

    rope

--- Assets/setRope.cs
+++ Assets/setRope.cs
@@ -11,6 +11,9 @@ public class setRope : MonoBehaviour
     public float Force;
     LineRenderer LR;
 
+    bool KazzoIsFollower = true;
+    Transform Folower;
+    Transform Leader;
     bool IsClose = false;
     private void Start()
     {
@@ -22,17 +25,32 @@ public class setRope : MonoBehaviour
         LR.SetPosition(1, Player2.position);
         LineLenght = Vector2.Distance(Player1.position, Player2.position);
         Force = LineLenght * Elasticite;
-        Player1.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        Player1.gameObject.GetComponent<Rigidbody2D>().AddForce(Player2.position * Force);
+        
+       
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            KazzoIsFollower = !KazzoIsFollower;
+        }
+        if(KazzoIsFollower)
+        {
+            Folower = Player1;
+            Leader = Player2;
+        }
+        else
+        {
+            Folower = Player2;
+            Leader = Player1;
+        }
 
+        Folower.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
         if (LineLenght<2&&IsClose==false)
         {
-            Player1.gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(0,0);
+            Folower.gameObject.GetComponent<Rigidbody2D>().velocity=new Vector2(0,0);
             IsClose = true;
         }
         else
         {
-            Player1.gameObject.GetComponent<Rigidbody2D>().AddForce(Player2.position * Force);
+            Folower.gameObject.GetComponent<Rigidbody2D>().AddForce(Leader.position * Force);
             IsClose = false;
         }
     }
