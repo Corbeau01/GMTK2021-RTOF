f5bf1e2926f838fd06a8a6161ddeeb414247981e
Richard <rail.rich@gmail.com>
2021-06-12 22:31:25 -0400

    Plus meilleur level

---
+++ Assets/script/DeathZone.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+	public Transform RespawnPoint;
+	public BoxCollider2D DeathZoneCollider;
+}
