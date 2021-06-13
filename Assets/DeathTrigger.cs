d801e745f87c43d797e07bf964aa567774a42a6a
Corbeau01 <corvus-corax@hotmail.com>
2021-06-12 22:11:19 -0400

    esayer fall

--- Assets/DeathTrigger.cs
+++ Assets/DeathTrigger.cs
@@ -19,6 +19,10 @@ public class DeathTrigger : MonoBehaviour
             {
                 Death = true;
             }
+            else
+            {
+                Destroy(collision.gameObject);
+            }
         }
 
     }
